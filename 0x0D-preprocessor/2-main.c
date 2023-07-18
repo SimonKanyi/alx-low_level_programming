51ba8ae7f73146786820acf3ba1d0d07cf268af7
SimonKanyi <simonmainaofficial1@gmail.com>
2023-07-18 03:42:49 -0700

    C - Preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+/**
+* main - prints the name of the file
+*
+* Return: Always 2 (Success)
+*/
 int main(void)
 {
     printf(__FILE__);
     printf("\n");
-    return 0;
+    return 2;
 }
