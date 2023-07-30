6c1a382c749c69504810293b6780ca0af2b2db91
SimonKanyi <simonmainaofficial1@gmail.com>
2023-07-30 03:31:54 -0700

    function_pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,43 +2,38 @@
 #include <stdlib.h>
 
 /**
- * This program prints the opcodes of its own instructions.
- * @argc: number of arguments
- * @argv: array of arguments
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
-    int bytes, i;
-    char *arr;
+	char *opc = (char *) main;
+	int i, nbytes;
 
-    if (argc != 2)
-    {
-        printf("Error: Invalid number of arguments\n");
-        exit(1);
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
 
-    bytes = atoi(argv[1]);
+	nbytes = atoi(argv[1]);
 
-    if (bytes < 0)
-    {
-        printf("Error: Negative number of bytes\n");
-        exit(2);
-    }
+	if (nbytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
 
-    arr = (char *)main;
+	for (i = 0; i < nbytes; i++)
+	{
+		printf("%02x", opc[i] & 0xFF);
+		if (i != nbytes - 1)
+			printf(" ");
+	}
 
-    for (i = 0; i < bytes; i++)
-    {
-        if (i == bytes - 1)
-        {
-            printf("%02hhx\n", arr[i]);
-            break;
-        }
-
-        printf("%02hhx ", arr[i]);
-    }
-
-    return 0;
+	printf("\n");
+	return (0);
 }
